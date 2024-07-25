c5d6639b88d538272ed630195ede3ecabf440e5f
Michael C. Saul <michael.saul@jax.org>
2024-07-25 09:48:06 -0700

    Created an experiment_time_encoding() function along with its complement of unit tests.

---
+++ R/experiment_time_encoding.R
@@ -0,0 +1,67 @@
+#' Experiment time encoding
+#'
+#' This function uses the date-time information in an annotation or an activity data set to compute experimental time.
+#' Experimental time is useful in aligning disparate studies and for re-encoding by something like date of birth.
+#'
+#' @param rawtimes raw times to be changed to experiment times.
+#' @param reftimes reference time or times for an experiment time encoding. Either length 1 or the same length as \code{rawtimes}.
+#' @param units units for the experiment time encoding (hours, days, weeks, months, or years).
+#' @param offset value given for the offset from 0 in the experiment in the same units as \code{units} (default: \code{0}).
+#' @returns a \code{vector} of experiment time in the given units.
+#' @keywords Envision
+
+experiment_time_encoding = function(rawtimes,
+                                    reftimes,
+                                    units = c("seconds","minutes","hours",
+                                              "days","weeks","months","years"),
+                                    offset = 0) {
+
+  # Ensuring essential packages are in the namespace
+  stopifnot(requireNamespace("lubridate", quietly = TRUE))
+
+  # Ensuring that times are all instants
+  stopifnot(lubridate::is.instant(rawtimes))
+  stopifnot(lubridate::is.instant(reftimes))
+
+  # Ensuring that the reftime value is numeric
+  stopifnot(is.numeric(offset))
+
+  # Ensuring that reftimes matches rawtimes
+  if (length(reftimes) == 1 & length(rawtimes) != 1) {
+    reftimes = rep(reftimes, times = length(rawtimes))
+  } else if (length(reftimes) == length(rawtimes) & length(reftimes) != 1) {
+    reftimes = reftimes
+  } else {
+    stop("rawtimes and reftimes are different lengths")
+  }
+
+  # Getting different numbers depending upon scale
+  if (units == "seconds") {
+    newtimes_raw <- as.double(lubridate::interval(reftimes, rawtimes) /
+                                1)
+  } else if (units == "minutes") {
+    newtimes_raw <- as.double(lubridate::interval(reftimes, rawtimes) /
+                                60)
+  } else if (units == "hours") {
+    newtimes_raw <- as.double(lubridate::interval(reftimes, rawtimes) /
+                                3600)
+  } else if (units == "days") {
+    newtimes_raw <- as.double(lubridate::interval(reftimes, rawtimes) /
+                                86400)
+  } else if (units == "weeks") {
+    newtimes_raw <- as.double(lubridate::interval(reftimes, rawtimes) /
+                                604800)
+  } else if (units == "months") {
+    newtimes_raw <- as.double(lubridate::interval(reftimes, rawtimes) /
+                                2629800)
+  } else if (units == "years") {
+    newtimes_raw <- as.double(lubridate::interval(reftimes, rawtimes) /
+                                31557600)
+  }
+
+  # Adding the reference time value
+  newtimes = newtimes_raw + offset
+
+  # Returning the result
+  return(newtimes)
+}
---
+++ tests/testthat/test-experiment_time_encoding.R
@@ -0,0 +1,214 @@
+# Unit tests for the experiment_time_encoding() function.
+
+require(lubridate)
+
+test_rawtimes = ymd_hms(paste0("2024-04-01 0",1:9, ":00:00"), tz = "UTC")
+test_reftime  = ymd_hms("2024-01-01 00:00:00", tz = "UTC")
+test_reftimes = rep(test_reftime, times = length(test_rawtimes))
+
+# test with only one reference time
+test_that("a single reference time works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes[1],
+                                        units = "seconds"),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800))
+})
+
+# test with only one reference time
+test_that("an unequal amount of raw and reference times throws an error when there are 2+ reftimes", {
+  expect_error(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes[1:2],
+                                        units = "seconds"),
+               "rawtimes and reftimes are different lengths")
+})
+
+
+# seconds
+test_that("seconds with no offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                           reftimes = test_reftimes,
+                           units = "seconds"),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800))
+})
+
+test_that("seconds with positive offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "seconds",
+                                        offset = 10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) + 10)
+})
+
+test_that("seconds with negative offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "seconds",
+                                        offset = -10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) - 10)
+})
+
+# minutes
+test_that("minutes with no offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "minutes"),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / 60)
+})
+
+test_that("minutes with positive offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "minutes",
+                                        offset = 10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / 60 + 10)
+})
+
+test_that("minutes with negative offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "minutes",
+                                        offset = -10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                  7891200, 7894800) / 60 - 10)
+})
+
+# hours
+test_that("hours with no offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "hours"),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (60 * 60))
+})
+
+test_that("hours with positive offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "hours",
+                                        offset = 10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (60 * 60) + 10)
+})
+
+test_that("hours with negative offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "hours",
+                                        offset = -10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (60 * 60) - 10)
+})
+
+# days
+test_that("days with no offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "days"),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (24 * 60 * 60))
+})
+
+test_that("days with positive offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "days",
+                                        offset = 10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (24 * 60 * 60) + 10)
+})
+
+test_that("days with negative offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "days",
+                                        offset = -10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (24 * 60 * 60) - 10)
+})
+
+# weeks
+test_that("weeks with no offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "weeks"),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (7 * 24 * 60 * 60))
+})
+
+test_that("weeks with positive offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "weeks",
+                                        offset = 10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (7 * 24 * 60 * 60) + 10)
+})
+
+test_that("weeks with negative offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "weeks",
+                                        offset = -10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (7 * 24 * 60 * 60) - 10)
+})
+
+# months
+test_that("months with no offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "months"),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / ((365.25 * 24 * 60 * 60) / 12))
+})
+
+test_that("months with positive offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "months",
+                                        offset = 10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / ((365.25 * 24 * 60 * 60) / 12) + 10)
+})
+
+test_that("months with negative offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "months",
+                                        offset = -10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / ((365.25 * 24 * 60 * 60) / 12) - 10)
+})
+
+# years
+test_that("years with no offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "years"),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (365.25 * 24 * 60 * 60))
+})
+
+test_that("years with positive offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "years",
+                                        offset = 10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (365.25 * 24 * 60 * 60) + 10)
+})
+
+test_that("years with negative offset works", {
+  expect_equal(experiment_time_encoding(rawtimes = test_rawtimes,
+                                        reftimes = test_reftimes,
+                                        units = "years",
+                                        offset = -10),
+               c(7866000, 7869600, 7873200, 7876800, 7880400, 7884000, 7887600,
+                 7891200, 7894800) / (365.25 * 24 * 60 * 60) - 10)
+})
