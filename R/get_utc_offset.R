81dec22bcabc8720fefbac6016cc476642bb1f5b
Michael C. Saul <michael.saul@jax.org>
2024-07-26 14:21:23 -0700

    removing lubridate dependency from get_utc_offset() function

--- R/get_utc_offset.R
+++ R/get_utc_offset.R
@@ -10,39 +10,39 @@
 #' @export
 #' @examples
 #' # Use with no second time stamp and an explicit time zone
-#' get_utc_offset(ts = lubridate::ymd_hms("2024-06-01 12:00:00", tz = "US/Pacific"))
+#' get_utc_offset(ts = as.POSIXct("2024-06-01 12:00:00", tzone = "US/Pacific"))
 #'
 #' # Use with a second time stamp and no explicit time zone.
-#' get_utc_offset(ts = lubridate::ymd_hms("2024-06-01 12:00:00"),
-#'                ts_utc = lubridate::ymd_hms("2024-06-01 05:00:00"))
+#' get_utc_offset(ts = as.POSIXct("2024-06-01 12:00:00"),
+#'                ts_utc = as.POSIXct("2024-06-01 05:00:00"))
 
 get_utc_offset = function(ts, ts_utc = NULL, as_numeric = FALSE) {
 
-  # Throwing an error if the R installation does not include lubridate
-  stopifnot(requireNamespace("lubridate", quietly = TRUE))
-
-  # Throwing an error if timestamp is not a timepoint
-  stopifnot(lubridate::is.timepoint(ts))
+  # Throwing an error if timestamp is not a POSIXct
+  stopifnot(inherits(ts, "POSIXct"))
 
   if (!is.null(ts_utc)) {
-    stopifnot(lubridate::is.timepoint(ts_utc))
+    stopifnot(inherits(ts_utc, "POSIXct"))
   } else {
-    ts_utc <- lubridate::with_tz(ts, tzone = "UTC")
+    ts_utc <- ts
+    attributes(ts_utc)$tzone <- "UTC"
   }
 
+  # Forcing the initial time zone to a new time zone
+  char_ts <- format(ts, "%Y-%m-%d %H:%M:%S")
+  force_utc_ts <- as.POSIXct(char_ts, tz = "UTC")
+  diff_secs <- as.double(force_utc_ts) - as.double(ts_utc)
+
   # Finding UTC time and using it to produce offset
-  offset_hr <- lubridate::as.period(lubridate::force_tz(ts, tzone = "UTC") -
-                                      ts_utc) %/% lubridate::hours(1)
-  offset_mn <- lubridate::as.period(lubridate::force_tz(ts, tzone = "UTC") -
-                                      ts_utc) %% lubridate::hours(1) %/%
-    lubridate::minutes(1)
+  offset_hr <- abs(diff_secs) %/% 3600 * sign(diff_secs)
+  offset_mn <- diff_secs %% 3600 %/% 60
 
   # Getting character or numeric output
   if (as_numeric) {
-    offset = offset_hr + offset_mn / 60
+    offset <- offset_hr + sign(diff_secs) * offset_mn / 60
   } else {
-    offset = paste0(formatC(offset_hr, width = 3, flag = "0+"), ":",
-                    formatC(abs(offset_mn), width = 2, flag = "0"))
+    offset <- paste0(formatC(offset_hr, width = 3, flag = "0+"), ":",
+                     formatC(abs(offset_mn), width = 2, flag = "0"))
   }
   # Returning a properly formatted UTC offset as a character vector
   return(offset)
--- tests/testthat/test-get_utc_offset.R
+++ tests/testthat/test-get_utc_offset.R
@@ -1,67 +1,90 @@
 # Unit tests for the get_utc_offset() function.
 
-require("lubridate")
-
 test_that("get_utc_offset() returns character for whole number standard time", {
-  expect_equal(get_utc_offset(lubridate::ymd_hms("2024-01-01 12:00:00",
-                                                 tz = "US/Pacific"),
+  expect_equal(get_utc_offset(as.POSIXct("2024-01-01 12:00:00",
+                                         tz = "US/Pacific"),
                               as_numeric = FALSE), "-08:00")
 })
 
 test_that("get_utc_offset() returns numeric for whole number standard time", {
-  expect_equal(get_utc_offset(lubridate::ymd_hms("2024-01-01 12:00:00",
-                                                 tz = "US/Pacific"),
+  expect_equal(get_utc_offset(as.POSIXct("2024-01-01 12:00:00",
+                                         tz = "US/Pacific"),
                               as_numeric = TRUE), -8)
 })
 
 test_that("get_utc_offset() returns character for whole number daylight time", {
-  expect_equal(get_utc_offset(lubridate::ymd_hms("2024-07-01 12:00:00",
-                                                 tz = "US/Pacific"),
+  expect_equal(get_utc_offset(as.POSIXct("2024-07-01 12:00:00",
+                                         tz = "US/Pacific"),
                               as_numeric = FALSE), "-07:00")
 })
 
 test_that("get_utc_offset() returns numeric for whole number daylight time", {
-  expect_equal(get_utc_offset(lubridate::ymd_hms("2024-07-01 12:00:00",
-                                                 tz = "US/Pacific"),
+  expect_equal(get_utc_offset(as.POSIXct("2024-07-01 12:00:00",
+                                         tz = "US/Pacific"),
                               as_numeric = TRUE), -7)
 })
 
 test_that("get_utc_offset() returns character for decimal standard time", {
-  expect_equal(get_utc_offset(lubridate::ymd_hms("2024-01-01 12:00:00",
-                                                 tz = "Canada/Newfoundland"),
+  expect_equal(get_utc_offset(as.POSIXct("2024-01-01 12:00:00",
+                                         tz = "Canada/Newfoundland"),
                               as_numeric = FALSE), "-03:30")
 })
 
 test_that("get_utc_offset() returns numeric for decimal standard time", {
-  expect_equal(get_utc_offset(lubridate::ymd_hms("2024-01-01 12:00:00",
-                                                 tz = "Canada/Newfoundland"),
+  expect_equal(get_utc_offset(as.POSIXct("2024-01-01 12:00:00",
+                                         tz = "Canada/Newfoundland"),
                               as_numeric = TRUE), -3.5)
 })
 
 test_that("get_utc_offset() returns character for decimal daylight time", {
-  expect_equal(get_utc_offset(lubridate::ymd_hms("2024-07-01 12:00:00",
-                                                 tz = "Canada/Newfoundland"),
+  expect_equal(get_utc_offset(as.POSIXct("2024-07-01 12:00:00",
+                                         tz = "Canada/Newfoundland"),
                               as_numeric = FALSE), "-02:30")
 })
 
 test_that("get_utc_offset() returns numeric for decimal daylight time", {
-  expect_equal(get_utc_offset(lubridate::ymd_hms("2024-07-01 12:00:00",
-                                                 tz = "Canada/Newfoundland"),
+  expect_equal(get_utc_offset(as.POSIXct("2024-07-01 12:00:00",
+                                         tz = "Canada/Newfoundland"),
                               as_numeric = TRUE), -2.5)
 })
 
+test_that("get_utc_offset() returns character for a positive offset decimal time", {
+  expect_equal(get_utc_offset(as.POSIXct("2024-01-01 12:00:00",
+                                         tz = "Asia/Kolkata"),
+                              as_numeric = FALSE), "+05:30")
+})
+
+test_that("get_utc_offset() returns numeric for a positive offset decimal time", {
+  expect_equal(get_utc_offset(as.POSIXct("2024-01-01 12:00:00",
+                                         tz = "Asia/Kolkata"),
+                              as_numeric = TRUE), 5.5)
+})
+
+test_that("get_utc_offset() returns character for a positive offset whole time", {
+  expect_equal(get_utc_offset(as.POSIXct("2024-01-01 12:00:00",
+                                         tz = "Asia/Almaty"),
+                              as_numeric = FALSE), "+06:00")
+})
+
+test_that("get_utc_offset() returns numeric for a positive offset whoe time", {
+  expect_equal(get_utc_offset(as.POSIXct("2024-01-01 12:00:00",
+                                         tz = "Asia/Almaty"),
+                              as_numeric = TRUE), 6)
+})
+
 test_that("get_utc_offset() works for a UTC time stamp passed to the function.", {
-  expect_equal(get_utc_offset(ts = lubridate::ymd_hms("2024-01-01 12:00:00",
-                                                      tz = "US/Pacific"),
-                              ts_utc = lubridate::ymd_hms("2024-01-01 20:00:00 UTC",
-                                                          tz = "UTC"),
+  expect_equal(get_utc_offset(ts = as.POSIXct("2024-01-01 12:00:00",
+                                              tz = "US/Pacific"),
+                              ts_utc = as.POSIXct("2024-01-01 20:00:00 UTC",
+                                                  tz = "UTC"),
                               as_numeric = TRUE), -8)
 })
 
-utc_datetimes = ymd_hms(paste0("2024-01-20 ",
-                              formatC(0:23, width=2, flag="0"),
-                              ":00:00"), tz = "UTC")
-pacific_datetimes = with_tz(utc_datetimes, tzone = "US/Pacific")
+utc_datetimes = as.POSIXct(paste0("2024-01-20 ",
+                               formatC(0:23, width=2, flag="0"),
+                               ":00:00"), tz = "UTC")
+pacific_datetimes = utc_datetimes
+attributes(pacific_datetimes)$tzone = "US/Pacific"
 
 test_that("get_utc_offset() works for a 24 hours of UTC time stamps passed to the function.", {
   expect_equal(get_utc_offset(ts = pacific_datetimes,
