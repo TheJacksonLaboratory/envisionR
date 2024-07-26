907c670a68ae16d10f1975d90475364874e61276
Michael C. Saul <michael.saul@jax.org>
2024-07-26 11:24:50 -0700

    Updated weeks_postnatal_seq() to remove dependency on lubridate

--- R/weeks_postnatal_seq.R
+++ R/weeks_postnatal_seq.R
@@ -2,27 +2,39 @@
 #'
 #' This function computes a vector of dates that start each postnatal weeks after a birth date.
 #' This is often useful for axis legends.
-#' @param dob Date of birth of the animal in a \code{ymd} format.
+#' @param dob Date of birth of the animal in a \code{Date} format.
 #' @param n_weeks Number of weeks as a \code{numeric}.
+#' @param quiet \code{logical} indicating whether or not to suppress errors (default: \code{FALSE})
 #' @returns A named vector of dates where the names are the postnatal weeks.
 #' @keywords Envision
 #' @export
 #' @examples
-#' weeks_postnatal_seq(dob = lubridate::ymd("2024-01-20"), n_weeks = 12)
+#' weeks_postnatal_seq(dob = as.Date("2024-01-20"), n_weeks = 12)
 
-weeks_postnatal_seq <- function(dob, n_weeks) {
+weeks_postnatal_seq <- function(dob, n_weeks, quiet = FALSE) {
 
-  # Throwing an error if lubridate is not available
-  stopifnot(requireNamespace("lubridate", quietly = TRUE))
-
-  # Throwing an error if DOB is not a Date
-  stopifnot(lubridate::is.Date(dob))
+  # Throwing an error if DOB is not a POSIXct or cannot be coerced to one
+  if (!inherits(dob, "Date") & !is.character(dob)) {
+    stop("dob is neither a Date nor a character vector")
+  } else if (is.character(dob)) {
+    date_dob <- try(as.Date(dob), silent = TRUE)
+    if (inherits(date_dob, "try-error")) {
+      stop("dob cannot be coerced to a Date")
+    } else {
+      if (!quiet) {
+        warning("dob coerced from character to Date type")
+      }
+      dob <- date_dob
+    }
+  }
 
   # Throwing an error if number of n_weeks is not numeric
   stopifnot(is.numeric(n_weeks))
 
   # Getting a sequence of postnatal days at which an animal is a certain age
-  pnds <- seq(from = dob, to = dob + 7 * floor(n_weeks), by = 7)
+  pnds <- seq(from = dob,
+              to = dob + 7 * floor(n_weeks),
+              by = 7)
   names(pnds) <- paste0("Postnatal Week ", seq_len(length(pnds)) - 1)
 
   # Returning the vector
--- tests/testthat/test-weeks_postnatal_seq.R
+++ tests/testthat/test-weeks_postnatal_seq.R
@@ -1,5 +1,6 @@
 # Unit tests for the weeks_postnatal_seq() function.
 
+
 pnw = structure(c(`Postnatal Week 0` = 19742, `Postnatal Week 1` = 19749,
                   `Postnatal Week 2` = 19756, `Postnatal Week 3` = 19763, `Postnatal Week 4` = 19770,
                   `Postnatal Week 5` = 19777, `Postnatal Week 6` = 19784, `Postnatal Week 7` = 19791,
@@ -8,8 +9,34 @@ pnw = structure(c(`Postnatal Week 0` = 19742, `Postnatal Week 1` = 19749,
                   `Postnatal Week 14` = 19840, `Postnatal Week 15` = 19847, `Postnatal Week 16` = 19854
 ), class = "Date")
 
-test_that("weeks_postnatal_seq() returns expected", {
-  expect_equal(weeks_postnatal_seq(dob = lubridate::ymd("2024-01-20"),
+test_that("weeks_postnatal_seq() returns expected results with as.Date() input for dob", {
+  expect_equal(weeks_postnatal_seq(dob = as.Date("2024-01-20"),
                       n_weeks = 16),
                pnw)
 })
+
+test_that("weeks_postnatal_seq() returns expected results with character input for dob when in quiet", {
+  expect_equal(weeks_postnatal_seq(dob = "2024-01-20",
+                                   n_weeks = 16,
+                                   quiet = TRUE),
+               pnw)
+})
+
+test_that("weeks_postnatal_seq() throws a warning for character vector", {
+  expect_warning(weeks_postnatal_seq(dob = "2024-01-20",
+                                     n_weeks = 16),
+               "dob coerced from character to Date type")
+})
+
+test_that("weeks_postnatal_seq() throws an error for character vector that cannot be coerced to a Date", {
+  expect_error(weeks_postnatal_seq(dob = "Hi there.",
+                                   n_weeks = 16),
+               "dob cannot be coerced to a Date")
+})
+
+test_that("weeks_postnatal_seq() throws an error when dob is neither Date nor character", {
+  expect_error(weeks_postnatal_seq(dob = FALSE,
+                                   n_weeks = 16),
+               "dob is neither a Date nor a character vector")
+})
+
