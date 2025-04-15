2b462f5cba27280d98a870ad2f6e0af91aad6a15
Michael C. Saul <michael.saul@jax.org>
2025-04-15 13:57:42 -0700

    Changed default font to D-DIN Expanded.

--- R/theme_jax_pub.R
+++ R/theme_jax_pub.R
@@ -12,7 +12,7 @@
 #' @export
 
 theme_jax_pub <- function(base_size = 11,
-                          base_family = "D-DIN",
+                          base_family = "D-DIN Expanded",
                           base_line_size = base_size / 22,
                           base_rect_size = base_size / 22) {
 
@@ -23,7 +23,7 @@ theme_jax_pub <- function(base_size = 11,
   if (!requireNamespace("sysfonts")) {
     stop("The sysfonts package is suggested for this function.")
   }
-  stoifnot(requireNamespace("ggplot2"))
+  stopifnot(requireNamespace("ggplot2"))
 
   # Loading the D-DIN font
   load_ddin_font(quietly = TRUE)
@@ -33,15 +33,15 @@ theme_jax_pub <- function(base_size = 11,
                            base_family = base_family,
                            base_line_size = base_line_size,
                            base_rect_size = base_rect_size) %+replace%
-    ggplot2::theme(text = ggplot2::element_text(family = "D-DIN", color = "#000100"), # All text is #000100 to allow editing
+    ggplot2::theme(text = ggplot2::element_text(family = base_family, color = "#000100"), # All text is #000100 to allow editing
                    panel.background = ggplot2::element_rect(fill = "#FEFFFF", color = NA), # Panel background is #FEFFFF to allow editing
                    panel.border = ggplot2::element_rect(fill = NA, color = "#000001"), # Panel border and ticks are #000001 to allow editing
                    panel.grid = ggplot2::element_line(color = "#FFFFFE"), # Panel grid is #FFFFFE to allow editing
                    panel.grid.minor = ggplot2::element_line(linewidth = rel(0.5)),
                    strip.background = ggplot2::element_rect(fill = "grey85", color = "#000100"),
-                   axis.text = ggplot2::element_text(family = "D-DIN Expanded", size = base_size / 1.375),
-                   axis.title = ggplot2::element_text(family = "D-DIN Expanded", size = base_size),
-                   title = ggplot2::element_text(family = "D-DIN Expanded", size = 12 * base_size / 11),
+                   axis.text = ggplot2::element_text(family = base_family, size = base_size / 1.375),
+                   axis.title = ggplot2::element_text(family = base_family, size = base_size),
+                   title = ggplot2::element_text(family = base_family, size = 12 * base_size / 11),
                    complete = TRUE
     )
   return(t)
