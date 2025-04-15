7dc0524a95eb2975e0234b4cfb6ae69ee01b8ed0
Michael C. Saul <michael.saul@jax.org>
2025-04-15 11:25:20 -0700

    Adding in condensed and expanded fonts.

--- R/load_ddin_font.R
+++ R/load_ddin_font.R
@@ -18,7 +18,7 @@ load_ddin_font <- function(quietly = FALSE) {
     stop("The sysfonts package is suggested for this function.")
   }
 
-  # Getting paths for fonts
+  # Getting paths for normal spacing fonts
   ddin_regular_path <- system.file("fonts", "D-DIN.ttf",
                                    package = "envisionR")
   ddin_bold_path <- system.file("fonts", "D-DIN-Bold.ttf",
@@ -26,11 +26,38 @@ load_ddin_font <- function(quietly = FALSE) {
   ddin_italic_path <- system.file("fonts", "D-DIN-Italic.ttf",
                                   package = "envisionR")
 
-  # Getting fonts
+  # Getting paths for condensed fonts
+  ddincond_regular_path <- system.file("fonts", "D-DINCondensed.ttf",
+                                   package = "envisionR")
+  ddincond_bold_path <- system.file("fonts", "D-DINCondensed-Bold.ttf",
+                                package = "envisionR")
+
+  # Getting paths for expanded fonts
+  ddinexp_regular_path <- system.file("fonts", "D-DINExp.ttf",
+                                   package = "envisionR")
+  ddinexp_bold_path <- system.file("fonts", "D-DINExp-Bold.ttf",
+                                package = "envisionR")
+  ddinexp_italic_path <- system.file("fonts", "D-DINExp-Italic.ttf",
+                                  package = "envisionR")
+
+  # Getting normal spacing fonts
   sysfonts::font_add("D-DIN",
                      regular = ddin_regular_path,
                      bold = ddin_bold_path,
                      italic = ddin_italic_path)
+
+  # Getting condensed fonts
+  sysfonts::font_add("D-DIN Condensed",
+                     regular = ddincond_regular_path,
+                     bold = ddincond_bold_path)
+
+  # Getting expanded fonts
+  sysfonts::font_add("D-DIN Expanded",
+                     regular = ddinexp_regular_path,
+                     bold = ddinexp_bold_path,
+                     italic = ddinexp_italic_path)
+
+
 
   # Running showtext_auto() to load the font.
   showtext::showtext_auto()
